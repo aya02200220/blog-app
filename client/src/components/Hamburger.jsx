6504ee3d2e1c6fe622888e71e9eaa38675faefac
Aya <pyon220@gmail.com>
2023-06-18 03:29:33 -0700

    editting side bar function

--- client/src/components/Hamburger.jsx
+++ client/src/components/Hamburger.jsx
@@ -1,4 +1,11 @@
 import * as React from "react";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { SearchWindow } from "./SearchWindow";
+import { UserContext } from "./UserContext";
+
+import toast from "react-hot-toast";
+
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -8,14 +15,34 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
 
-export default function Hamburger() {
+import MenuIcon from "@mui/icons-material/Menu";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ArticleIcon from "@mui/icons-material/Article";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+
+export default function Hamburger({ userName }) {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+
   const [isOpen, setIsOpen] = React.useState({
     right: false,
   });
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+    setIsOpen({ ...isOpen, right: false }); // ログアウト後にメニューを閉じる
+    toast.success("You are logged out!");
+  }
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -29,38 +56,68 @@ export default function Hamburger() {
   };
 
   const list = (anchor) => (
-    <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    <>
+      <Box
+        sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+          <NavLink to={userName ? "/create" : "/login"}>
+            <ListItem disablePadding sx={{ marginBottom: "4px" }}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <BorderColorIcon color="4e575f" sx={{ marginLeft: "10px" }} />
+                </ListItemIcon>
+                <ListItemText primary={"Create Post"} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+
+          <NavLink to={"/favorite"}>
+            <ListItem disablePadding sx={{ marginBottom: "4px" }}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <BookmarkIcon color="4e575f" sx={{ marginLeft: "10px" }} />
+                </ListItemIcon>
+                <ListItemText primary={"Bookmark"} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+
+          <NavLink to={"/favorite"}>
+            <ListItem disablePadding sx={{ marginBottom: "4px" }}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <ArticleIcon color="4e575f" sx={{ marginLeft: "10px" }} />
+                </ListItemIcon>
+                <ListItemText primary={"Your Posts"} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        </List>
+
+        <Divider />
+        {userName && (
+          <List>
+            <NavLink to={"/favorite"}>
+              <ListItem disablePadding sx={{ marginBottom: "4px" }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <PersonOutlineIcon
+                      color="4e575f"
+                      sx={{ marginLeft: "10px" }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary={"Account Settings"} />
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+          </List>
+        )}
+      </Box>
+    </>
   );
 
   return (
@@ -75,7 +132,41 @@ export default function Hamburger() {
             open={isOpen[anchor]}
             onClose={toggleDrawer(anchor, false)}
           >
+            <Box
+              sx={{ marginTop: "10px", display: { xs: "block", md: "none" } }}
+            >
+              <SearchWindow />
+              <Divider sx={{ paddingTop: "10px" }} />
+            </Box>
+
             {list(anchor)}
+            {userName && (
+              <Box sx={{ marginTop: "0px" }}>
+                <Button
+                  onClick={logout}
+                  sx={{ marginTop: "15px", width: "100%", height: "40px" }}
+                  variant="contained"
+                  color="error"
+                  startIcon={<LogoutIcon />}
+                >
+                  Sign out
+                </Button>
+              </Box>
+            )}
+            {!userName && (
+              <Box sx={{ marginTop: "0px" }}>
+                <NavLink to={"/login"} onClick={toggleDrawer("right", false)}>
+                  <Button
+                    sx={{ marginTop: "15px", width: "100%", height: "40px" }}
+                    variant="contained"
+                    color="success"
+                    startIcon={<LoginIcon />}
+                  >
+                    Sign In
+                  </Button>
+                </NavLink>
+              </Box>
+            )}
           </Drawer>
         </React.Fragment>
       ))}
--- client/src/components/Header2.jsx
+++ client/src/components/Header2.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Hamburger from "./Hamburger";
+import { SearchWindow } from "./SearchWindow";
 
 import styles from "../styles/main.module.scss";
 
@@ -28,45 +29,45 @@ import BorderColorIcon from "@mui/icons-material/BorderColor";
 
 import MoreIcon from "@mui/icons-material/MoreVert";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
+// const Search = styled("div")(({ theme }) => ({
+//   position: "relative",
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   "&:hover": {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginRight: theme.spacing(2),
+//   marginLeft: 0,
+//   width: "100%",
+//   [theme.breakpoints.up("sm")]: {
+//     marginLeft: theme.spacing(3),
+//     width: "auto",
+//   },
+// }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: "inherit",
+//   "& .MuiInputBase-input": {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create("width"),
+//     width: "100%",
+//     [theme.breakpoints.up("md")]: {
+//       width: "20ch",
+//     },
+//   },
+// }));
 
 export default function Header() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -289,35 +290,22 @@ export default function Header() {
             <NavLink to="/">MERN-Blog</NavLink>
           </Typography>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon sx={{ color: "#4e575f" }} />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-                sx={{
-                  color: "#4e575f",
-                  border: "1px solid #ccc",
-                  borderRadius: "5px",
-                }}
-              />
-            </Search>
+            <SearchWindow />
           </Box>
 
           <Box sx={{ flexGrow: 1 }} />
           {userName && (
             <IconButton
-              size="large"
-              edge="end"
+              // size="large"
+              // edge="end"
               aria-label="account of current user"
               aria-controls={menuId}
               aria-haspopup="true"
               onClick={handleProfileMenuOpen}
               color="4e575f"
-              sx={{
-                marginRight: "10px",
-              }}
+              // sx={{
+              //   marginRight: "10px",
+              // }}
             >
               <LoginIcon
                 firstLetter={firstName.charAt(0)}
@@ -413,17 +401,6 @@ export default function Header() {
             >
               <Hamburger userName={userName} />
             </IconButton>
-            {/* <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-              <Hamburger userName={userName} />
-            </IconButton> */}
           </Box>
         </Toolbar>
       </AppBar>
