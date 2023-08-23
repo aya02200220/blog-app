7c31a10095658686c86d6f268a9bf2bc331096a9
Aya <pyon220@gmail.com>
2023-08-23 03:34:32 -0700

    added action when deleting a post on readin list page

--- client/src/components/Pages/FavPost.jsx
+++ client/src/components/Pages/FavPost.jsx
@@ -16,6 +16,7 @@ import {
 } from "@mui/material";
 import { alpha } from "@mui/material/styles";
 import CloseIcon from "@mui/icons-material/Close";
+import Collapse from "@mui/material/Collapse";
 
 import { format, differenceInDays, formatDistanceToNow } from "date-fns";
 import { Link } from "react-router-dom";
@@ -46,7 +47,7 @@ export const FavPost = ({
       return format(createdDate, "MMM d, yyyy");
     }
   };
-
+  const [showPost, setShowPost] = useState(true);
   const [openDialog, setOpenDialog] = useState(false);
   const handleOpenDialog = () => {
     setOpenDialog(true);
@@ -59,8 +60,12 @@ export const FavPost = ({
   const handleConfirmRemove = () => {
     handleCloseDialog();
     // ここで削除処理を実行します
-    handleRemoveFromFavorites();
-    handleRemoveClick();
+    setShowPost(false);
+    setTimeout(() => {
+      handleRemoveFromFavorites();
+    }, 1000);
+
+    // handleRemoveClick();
   };
 
   const handleRemoveFromFavorites = async () => {
@@ -80,133 +85,135 @@ export const FavPost = ({
         openDialog={openDialog}
         handleConfirmRemove={handleConfirmRemove}
       />
-      <Box
-        sx={{
-          position: "relative",
-        }}
-      >
-        <IconButton
-          // size="small"
+      <Collapse in={showPost}>
+        <Box
           sx={{
-            position: "absolute",
-            right: 3,
-            top: 5,
-            color: "#c9c9c9",
-            backgroundColor: alpha("#fff", 0.3),
-            height: "25px",
-            width: "25px",
-            zIndex: 1,
+            position: "relative",
           }}
-          onClick={handleOpenDialog}
         >
-          <CloseIcon />
-        </IconButton>
-        <Link to={`/post/${_id}`} state={"/favorites"}>
-          <Box
+          <IconButton
+            // size="small"
             sx={{
-              maxWidth: "100%",
-              height: { xs: "290px", sm: "400px", md: "110px" },
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              backgroundColor: "#fff",
-              borderBottom: "solid 0.2px #c2c2c2",
-              position: "relative",
+              position: "absolute",
+              right: 3,
+              top: 5,
+              color: "#c9c9c9",
+              backgroundColor: alpha("#fff", 0.3),
+              height: "25px",
+              width: "25px",
+              zIndex: 1,
             }}
+            onClick={handleOpenDialog}
           >
+            <CloseIcon />
+          </IconButton>
+          <Link to={`/post/${_id}`} state={"/favorites"}>
             <Box
               sx={{
-                height: { xs: "62%", sm: "70%", md: "100%" },
-                width: { xs: "100%", md: "30%" },
-                maxWidth: { xs: "100%", md: "30%" },
-                minWidth: { xs: "100%", md: "30%" },
-                objectFit: "cover",
+                maxWidth: "100%",
+                height: { xs: "290px", sm: "400px", md: "110px" },
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                backgroundColor: "#fff",
+                borderBottom: "solid 0.2px #c2c2c2",
+                position: "relative",
               }}
-              component="img"
-              src={"http://localhost:4000/" + cover}
-            ></Box>
-            <Box sx={{ display: "flex", flexDirection: "column" }}>
-              <Link to={`/viewall`}>
-                <IconButton
-                  size="large"
-                  edge="end"
-                  color="4e575f"
-                  sx={{
-                    p: 0,
-                    ml: 2,
-                    mt: 1,
-                    pr: 2,
-                    borderRadius: "50px 5px 5px 50px",
-                    backgroundColor: alpha("#fff", 0.4),
-                  }}
-                >
-                  <LoginIcon
-                    firstLetter={author?.firstName.charAt(0)}
-                    lastLetter={author?.lastName.charAt(0)}
-                    userIcon={authorProfile?.user.userIcon}
-                    sx={{ padding: 0 }}
-                  />
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      ml: 1.5,
-                      justifyContent: "left",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        fontSize: "16px",
-                        fontWeight: "600",
-                        lineHeight: "16px",
-                      }}
-                    >
-                      {author?.firstName} {author?.lastName}
-                    </Typography>
-                    <Typography
-                      sx={{
-                        fontSize: "14px",
-                        fontWeight: "400",
-                        textAlign: "left",
-                        lineHeight: "15px",
-                      }}
-                    >
-                      {createdAt && formatDate(createdAt)}
-                    </Typography>
-                  </Box>
-                </IconButton>
-              </Link>
-
+            >
               <Box
                 sx={{
-                  flexGrow: 1,
-                  // border: "solid 1px black",
-                  width: "100%",
-                  p: 2,
-                  pt: 1,
+                  height: { xs: "62%", sm: "70%", md: "100%" },
+                  width: { xs: "100%", md: "30%" },
+                  maxWidth: { xs: "100%", md: "30%" },
+                  minWidth: { xs: "100%", md: "30%" },
+                  objectFit: "cover",
                 }}
-              >
-                <Typography
-                  sx={{
-                    color: "#404040",
-                    fontWeight: "500",
-                    fontSize: { xs: "16px", sm: "20px" },
-                    lineHeight: { xs: "16px", sm: "20px" },
-                    minHeight: { xs: "34px", sm: "41px" },
-                    wordBreak: "break-word",
+                component="img"
+                src={"http://localhost:4000/" + cover}
+              ></Box>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Link to={`/viewall`}>
+                  <IconButton
+                    size="large"
+                    edge="end"
+                    color="4e575f"
+                    sx={{
+                      p: 0,
+                      ml: 2,
+                      mt: 1,
+                      pr: 2,
+                      borderRadius: "50px 5px 5px 50px",
+                      backgroundColor: alpha("#fff", 0.4),
+                    }}
+                  >
+                    <LoginIcon
+                      firstLetter={author?.firstName.charAt(0)}
+                      lastLetter={author?.lastName.charAt(0)}
+                      userIcon={authorProfile?.user.userIcon}
+                      sx={{ padding: 0 }}
+                    />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        ml: 1.5,
+                        justifyContent: "left",
+                      }}
+                    >
+                      <Typography
+                        sx={{
+                          fontSize: "16px",
+                          fontWeight: "600",
+                          lineHeight: "16px",
+                        }}
+                      >
+                        {author?.firstName} {author?.lastName}
+                      </Typography>
+                      <Typography
+                        sx={{
+                          fontSize: "14px",
+                          fontWeight: "400",
+                          textAlign: "left",
+                          lineHeight: "15px",
+                        }}
+                      >
+                        {createdAt && formatDate(createdAt)}
+                      </Typography>
+                    </Box>
+                  </IconButton>
+                </Link>
 
-                    display: "-webkit-box",
-                    WebkitBoxOrient: "vertical",
-                    WebkitLineClamp: 2, // 行数指定
-                    overflow: "hidden",
+                <Box
+                  sx={{
+                    flexGrow: 1,
+                    // border: "solid 1px black",
+                    width: "100%",
+                    p: 2,
+                    pt: 1,
                   }}
                 >
-                  {title}
-                </Typography>
+                  <Typography
+                    sx={{
+                      color: "#404040",
+                      fontWeight: "500",
+                      fontSize: { xs: "16px", sm: "20px" },
+                      lineHeight: { xs: "16px", sm: "20px" },
+                      minHeight: { xs: "34px", sm: "41px" },
+                      wordBreak: "break-word",
+
+                      display: "-webkit-box",
+                      WebkitBoxOrient: "vertical",
+                      WebkitLineClamp: 2, // 行数指定
+                      overflow: "hidden",
+                    }}
+                  >
+                    {title}
+                  </Typography>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        </Link>
-      </Box>
+          </Link>
+        </Box>
+      </Collapse>
     </>
   );
 };
@@ -222,7 +229,7 @@ export const ConfirmDeleteDialog = (props) => {
         "& .MuiDialog-paper": {
           width: "30%",
           maxWidth: "unset",
-          minWidth: "200px", // ここにminWidthを追加
+          minWidth: "200px",
         },
       }}
     >
