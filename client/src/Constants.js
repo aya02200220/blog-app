d60b21f27f06376b0593623cc5e2fdc5db3e6c1d
Aya <pyon220@gmail.com>
2023-08-26 13:37:08 -0700

    added deployed server url

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,1 +1,3 @@
-export const SERVER_URL = "http://localhost:4000";
+// export const SERVER_URL = "http://localhost:4000";
+export const SERVER_URL =
+  "https://noir-rose-server-978tyrp2c-aya02200220.vercel.app";
