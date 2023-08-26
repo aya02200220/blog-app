4b80e7def3da06efab19a04939989da89e1f13c8
Aya <pyon220@gmail.com>
2023-08-26 14:15:53 -0700

    changed the CORS from local to deployed URL

--- api/index.js
+++ api/index.js
@@ -16,7 +16,10 @@ const cors = require("cors");
 const { log } = require("console");
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(
+  cors({ credentials: true, origin: "https://blog-app-du13.vercel.app" })
+);
 app.use(express.json());
 app.use(cookieParser());
 const salt = bcrypt.genSaltSync(10);
