2f5456af1e26cc2ef9dfb8be7d5fbf7ce4881214
Aya <pyon220@gmail.com>
2023-09-06 16:28:26 -0700

    added start node

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,3 +1,2 @@
 // export const SERVER_URL = "http://localhost:4000";
-export const SERVER_URL =
-  "https://noir-rose-server-hzekd940l-aya02200220.vercel.app";
+export const SERVER_URL = "https://blog-app-mqaa.onrender.com";
