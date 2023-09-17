f1948422a6269bacb4e3f7bf22bcca5b69d7b670
Aya <pyon220@gmail.com>
2023-09-16 22:14:25 -0700

    added vercel urls to allowed origins

--- api/index.js
+++ api/index.js
@@ -17,7 +17,9 @@ const { log } = require("console");
 const app = express();
 
 const allowedOrigins = [
-  "https://noir-rose-munj9evek-aya02200220.vercel.app",
+  "https://noir-rose-r4o0l6ukr-aya02200220.vercel.app",
+  "https://www.noir-rose.com",
+  "https://noir-rose.vercel.app",
   "http://localhost:5173",
 ];
 
